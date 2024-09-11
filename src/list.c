decf9120fe8958cef58cf95f1f840829ea0f27b3
vaughnh2022 <vhartzell@luc.edu>
2024-09-10 19:33:48 -0500

    homework one

--- src/list.c
+++ src/list.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include "list.h"
 
-void list_add(struct list_element *list_head,struct list_element *new_element){
-   new_element->next=list_head->next;
-   list_head->next=new_element;
+void list_add(struct list_element *list_head,struct list_element *new_element){  
+   struct list_element *cursor=list_head;
+   while(cursor->next!=null){
+      cursor=cursor->next;
+   }
+   cursor->next=new_element;
 }
 void list_remove(struct list_element *list_head,struct list_element *old_element){
-   struct list_element cursor = list_head;
-   if(list_head==old_element){
-   list_head=list_head->next;
+   struct list_element *cursor=list_head;
+   if(cursor==old_element){
+      cursor=cursor->next;
+      list_head=cursor;
    } else {
-      while(cursor->next!=old_element){
+      while(cursor->next!=NULL&&cursor->next!=old_element){
          cursor=cursor->next;
       }
-      cursor->next=cursor->next->next;
-   }
+      if(cursor->next!=NULL){
+         cursor->next=cursor->next->next;
+      }
 }
 int main(){
    struct list_element c = {NULL,0};
